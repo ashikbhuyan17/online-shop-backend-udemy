69b1c76790425191e362e23195dab66d3196cde9
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-10-10 12:46:17 +0600

    Fix Issue

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,18 @@
 const admin = require('../firebase')
 
 exports.authCheck = async (req, res, next) => {
-    try {
-      const firebaseUser = await admin
-        .auth()
-        .verifyIdToken(req.headers.authtoken);
-      req.user = firebaseUser;
-      next();
-    } catch (error) {
-      console.log(error);
-      res.status(401).json({
-        message: "Invalid or expire token",
-      });
-    }
-  };
+  console.log("...............", req.headers)
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    console.log("firebaseUser", firebaseUser);
+    req.user = firebaseUser;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      message: "Invalid or expire token",
+    });
+  }
+};
--- server.js
+++ server.js
@@ -37,5 +37,5 @@ readdirSync('./routes').map((r) => app.use("/api", require('./routes/' + r)))
 
 
 // PORT
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`Server is running on port! ${port}`))
