6822051f8c371159ddb4c85e5ab11eba0b5ea76c
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-10-12 17:51:49 +0600

    Product

--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,13 @@ const {
     create,
     listAll,
     totalProduct,
-    // remove,
-    // read,
-    // update,
-    // list,
-    // productStar,
-    // relatedProduct,
-    // searchFilters,
+    remove,
+    read,
+    update,
+    list,
+    productStar,
+    relatedProduct,
+    searchFilters,
 } = require("../controllers/product");
 const { authCheck, adminCheck } = require("../middlewares/auth");
 const router = express.Router();
@@ -17,11 +17,12 @@ const router = express.Router();
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/total", totalProduct);
 router.get("/products/:count", listAll);
-// router.delete("/product/:slug", authCheck, adminCheck, remove);
-// router.get("/product/:slug", read);
-// router.put("/product/:slug", authCheck, adminCheck, update);
-// router.post("/products", list);
-// router.put("/product/star/:productId", authCheck, productStar);
-// router.get("/product/related/:productId", relatedProduct);
-// router.post("/search/filters", searchFilters);
+router.delete("/product/:slug", authCheck, adminCheck, remove);
+router.get("/product/:slug", read);
+router.put("/product/:slug", authCheck, adminCheck, update);
+router.post("/products", list);
+router.put("/product/star/:productId", authCheck, productStar);
+router.get("/product/related/:productId", relatedProduct);
+router.post("/search/filters", searchFilters);
+
 module.exports = router;
